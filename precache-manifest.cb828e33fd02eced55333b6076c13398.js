931b2d2ee2c97c36d688598142456d2c95413a25
arpan2798 <aksatpathi027@gmail.com>
2020-10-13 09:44:37 +0530

    Updates

---
+++ precache-manifest.cb828e33fd02eced55333b6076c13398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7106b84d17553c980160f4d7afa648",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "37be637c1fe943afbc5d",
+    "url": "/monsters-rolodex/static/css/main.c2c63c9e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37be637c1fe943afbc5d",
+    "url": "/monsters-rolodex/static/js/main.4d4de171.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
